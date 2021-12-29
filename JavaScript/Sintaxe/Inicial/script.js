e4c342ef8d33afa2b9e1351f53e7cbd5b173ce64
Daviafer <daviafer1@gmail.com>
2021-12-29 19:03:25 -0300

    Alguns scripts iniciais

--- JavaScript/Sintaxe/Inicial/script.js
+++ JavaScript/Sintaxe/Inicial/script.js
@@ -17,6 +17,11 @@ var nome = 'fulano';
 console.log(nome);
 console.log(typeof(nome));
 
+var nomeInicial = 'Nome';
+var nomeFinal = 'Final';
+var nomeTotal = `Nome completo: ${nomeInicial} ${nomeFinal}`;
+console.log(nomeTotal);
+
 // função //
 var funcao = function(){
   console.log(funcao);
@@ -24,13 +29,15 @@ var funcao = function(){
 };
 */
 /*
-// var  - let  - const //
-var variavel = 'Beleza'; // escopo global e local //
+// var (escopo global e local)  - let (escopo local)  - const //
+
+var variavel = 'Beleza';
 variavel = 1;
 console.log(variavel);
-let variavel2 = 'Beleza'; // escopo local //
+let variavel2 = 'Beleza';
 variavel2 = 1;
 console.log(variavel2);
+
 const constante = 'Beleza'; // escopo local, somente leitura, valor inicial obrigatório //
 constante = 1;
 console.log(constante);
@@ -38,11 +45,28 @@ console.log(constante);
 // Escopo global //
 var escopoGlobal = 'global';
 console.log(escopoGlobal);
+
 // Escopo local //
 function escopoLocal(){
   let escopoLocalInterno = 'local';
   console.log(escopoLocalInterno);
 }
+
+//OUTRO EXEMPLO:  var , let
+var a = 1;
+var b = 2;
+
+if (a === 1) {
+  var a = 11;   //alterando valor (será alterado)
+  let b = 22;   //alterando valor (será alterado no escopo do if)
+
+  console.log(a);   // valor será 11
+  console.log(b);   // valor será 22
+}
+console.log(a);     // valor será 11
+console.log(b);     //valor voltará a ser 2, está fora do if 
+
+
 escopoLocal();
 */
 /*

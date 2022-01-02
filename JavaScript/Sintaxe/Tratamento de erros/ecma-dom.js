f3d3929b54d41c6d047017436e719ef109caf885
Daviafer <daviafer1@gmail.com>
2022-01-01 22:10:30 -0300

    Tratamento de erros

---
+++ JavaScript/Sintaxe/Tratamento de erros/ecma-dom.js
@@ -0,0 +1,13 @@
+/* ECMAScript Error */
+/* Erros que ocorrem em tempo de execução */
+/* Composto:
+  - Mensagem
+  - Nome
+  - Linha
+  - Call Stack
+*/
+
+/* DOMException */
+/* Erros relacionados ao Ducument Object Model - DOM */
+/* Erros da estrutura da arvore de elementos */
+
